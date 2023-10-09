49b8efb0419b1d3b5356e4fe7bd3af6e116bb593
stuart craigen <s.craigen@mac.com>
2023-10-09 14:19:21 +0100

    wip

--- src/test/java/com/example/springboot/AboutControllerTests.java
+++ src/test/java/com/example/springboot/AboutControllerTests.java
@@ -1,12 +1,18 @@
 package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.when;
 import static io.restassured.RestAssured.with;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = Application.class)
 public class AboutControllerTests {
     @Test
     public void testBark() {
